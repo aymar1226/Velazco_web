bb601cdf984a3a1ec33cbdb06878870e08f25d98
JACK <jackaymar1226@gmail.com>
2022-12-02 12:32:31 -0500

    Añadir archivos a proyecto

---
+++ src/main/java/com/utp/spring/controllers/ContactoController.java
@@ -0,0 +1,68 @@
+package com.utp.spring.controllers;
+
+import com.utp.spring.services.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+
+@Controller
+public class ContactoController {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private IUsuarioService usuarioService;
+
+    @GetMapping("/contacto")
+    public String showContactForm(Model modelo, HttpSession session) {
+        if(session.getAttribute("idusuario")!=null){
+            modelo.addAttribute(usuarioService.findbyId(Long.parseLong(session.getAttribute("idusuario").toString())).get());
+        }
+        modelo.addAttribute("rolsesion",session.getAttribute("rolusuario"));
+        return "contacto_form";
+    }
+
+
+    @PostMapping("/contacto")
+    public String submitContact(HttpServletRequest request) throws MessagingException, UnsupportedEncodingException {
+        String fullname = request.getParameter("fullname");
+        String email = request.getParameter("email");
+        String subject = request.getParameter("subject");
+        String content = request.getParameter("content");
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+
+        String mailSubject = fullname + " has sent a message";
+        String mailContent = "<p><b>Sender Name:</b> " + fullname + "</p>";
+        mailContent += "<p><b>Sender E-mail:</b>  " + email + "</p>";
+        mailContent += "<p><b>Subject:</b>  " + subject + "</p>";
+        mailContent += "<p><b>Content:</b>  " + content + "</p>";
+        mailContent += "<hr><img src='cid:logoImage' />";
+
+
+        helper.setFrom("bonihd48@gmail.com", "Probando el java-meil");
+        helper.setTo("jorgebonifazcampos4848@gmail.com");
+
+        helper.setSubject(mailSubject);
+        helper.setText(mailContent, true);
+
+
+        mailSender.send(message);
+
+        return "message";
+    }
+}
