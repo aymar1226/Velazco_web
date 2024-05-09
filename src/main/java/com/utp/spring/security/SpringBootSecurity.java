b5748ca0b644936127524e8e3608e51b6cd2ee1d
JACK <jackaymar1226@gmail.com>
2024-05-08 23:37:08 -0500

    ...

--- src/main/java/com/utp/spring/security/SpringBootSecurity.java
+++ src/main/java/com/utp/spring/security/SpringBootSecurity.java
@@ -1,9 +1,10 @@
-package com.utp.spring.services;
+package com.utp.spring.security;
 
 import com.utp.spring.models.entity.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.http.HttpSession;
@@ -23,6 +25,7 @@ public class SpringBootSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
 
 
     @Override
